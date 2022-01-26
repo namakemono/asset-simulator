99ace27ede305cc0a1af69faeb53f280ce0499bd
Masataka Nishimori <nishimori-m@mnes.org>
2022-01-26 16:04:42 +0900

    各種パラメータの表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const normRand = function (mu:number, sigma:number) {
+const gauss = function (mu:number, sigma:number) {
     const a = 1 - Math.random();
     const b = 1 - Math.random();
     const c = Math.sqrt(-2 * Math.log(a));
@@ -11,48 +11,42 @@ const normRand = function (mu:number, sigma:number) {
     }
 };
 
-interface Fund {
-    name: string; // 銘柄
-    value: number; // 現在の資産
-    // cost: number; // 毎年の投資額(万円)
-    mu: number; // リターン(平均)
-    sigma: number; // リスク(標準偏差)
-};
-
 interface Props {
-    age: number;        // 現在の年齢
+    age: number; // 現在の年齢
     expenditure: number; // 年間支出(万円)
-    // funds: Fund[];   // ポートフォリオ 
+    value: number; // 株式資産(万円)
+    mu: number; // リターン
+    sigma: number; // リスク
+    money: number; // 預金額(万円)
+    salary: number; // 給料（手取り)
+    years: number; // あと何年働くか
+    invest: number; // 年間投資額(万円)
+    emergency: number; // 生活防衛費(万円)
 }
 
 interface Result {
     survived: boolean;
-    history: { t: number, x: number, y: number, r: number }[]
+    history: { t: number, age: number, x: number, y: number, r: number }[]
 }
 const simulate = (props: Props): Result => {
     let res: Result = {
         survived: true, // 目標の年数生き延びれたかどうか
         history: []
     };
-    let x0 = 300; // 初期投資額(万円)
-    let y0 = 100; // 現金 or 債権(万円)
+    let x = props.value; // 初期投資額(万円)
+    let y = props.money; // 現金
     let p = props.expenditure; // 年間の支出(万円)
-    let iDeCo = 14.4; // iDeCo(万円)
-    let iDeCoAsset: Fund = {name: "iDeCo", value:0, mu:0.072, sigma:0.25};
-    let q = 500; // 給料(手取り)
-    let w = 150; // 生活防衛費
-    let s = 240; // 年間の投資額(万円)
-    let mu = 0.072; // リターン(平均)
-    let sigma = 0.25; // リスク(標準偏差)
-    let k = 10; // あと何年働くか(目標金額のほうが良いかも)
+    let q = props.salary; // 給料(手取り)
+    let w = props.emergency; // 生活防衛費
+    let s = props.invest; // 年間の投資額(万円)
+    let mu = props.mu; // リターン(平均)
+    let sigma = props.sigma; // リスク(標準偏差)
+    let k = props.years; // あと何年働くか(目標金額のほうが良いかも)
     let T = 50; // 何年先までシミュレーションしたいか
-    // TODO(nishimori-m): 年金がもらえる年齢
-    // TODO(nishimori-m): 
-    let x = x0; // 投資額
-    let y = y0; // 現金
+
     for (let t = 0; t < T; ++t) {
         const age = props.age + t;
-        const r = normRand(mu, sigma);
+        const r = Math.max(-0.9, gauss(mu, sigma));
         if ((y < 0) || (x < 0)) {
             res.survived = false; 
             break;
@@ -60,24 +54,21 @@ const simulate = (props: Props): Result => {
         // 給料
         if (t < k) {
             y += q;
-        }
-        // iDeCo
-        if (age < 60) {
-            y -= iDeCo;
-            iDeCoAsset.value = iDeCoAsset.value * (1 + r) + iDeCo;
-        } else if (age === 60) {
-            y += iDeCoAsset.value;
-        }
-        // 国民年金(厚生年金) + 健康保険
-        if (age < 65) {
-            y -= 30; 
-        } else { // 年金の受け取り
-            y += 100;
+        } else {
+            // 国民年金(厚生年金) + 健康保険
+            if (age < 65) {
+                y -= 30; 
+            } else { // 年金の受け取り
+                y += 100;
+            }
         }
         // 投資 or 切り崩し
         if (y < 0.2 * (x+y)) { // 債権の割合が20%を切っている
             y += p;
             x -= p;
+        } else if (x > 0.8 * (x+y)) { // 株式持ちすぎ
+            y -= p;
+            x += p;
         } else if (y >= w + s) { // 現金に余裕があるので投資
             y -= s;
             x += s;
@@ -94,8 +85,10 @@ const simulate = (props: Props): Result => {
             y -= p;
         }
         // 複利
-        x = x * (1 + r);
-        res.history.push({ t, x, y, r });
+        if (x > 0) {
+            x *= 1 + r;
+        }
+        res.history.push({ t, age, x, y, r });
     }
     return res;
 }
@@ -110,37 +103,41 @@ const calcWinRatio = (results:Result[]):number => {
     });
     return numWin / numTotal;
 }
+const parse = (x:string):number => {
+    if (x.length === 0) {
+        return 0;
+    } else {
+        return parseInt(x);
+    }
+}
 const App = () => {
-    const [age, setAge] = React.useState<number>(30);
-    const [expenditure, setExpenditure] = React.useState<number>(300);
-    const [funds, setFunds] = React.useState<Fund[]>([{
-        name: "海外株式",
-        value: 1000,
-        mu: 0.072,
-        sigma: 0.25
-    }]);
+    const [age, setAge] = React.useState<number>(25);
+    const [expenditure, setExpenditure] = React.useState<number>(250);  // 年間の支出
+    const [salary, setSalary] = React.useState<number>(500);        // 年収(手取り)
+    const [invest, setInvest] = React.useState<number>(150);        // 年間投資額(万円)
+    const [years, setYears] = React.useState<number>(20);           // あと何年働きたい?
+    const [money, setMoney] = React.useState<number>(300);          // 現在の預金額(万円)
+    const [value, setValue] = React.useState<number>(1000);         // 初期投資額
+    const [mu, setMu] = React.useState<number>(0.08);              // リターン
+    const [sigma, setSigma] = React.useState<number>(0.2);         // リスク
+    const [emergency, setEmergency] = React.useState<number>(300);  // 生活防衛資金(万円)
     let results = [];
-    for (let k = 0; k < 100; ++k) {
+    for (let k = 0; k < 1000; ++k) {
         results.push(simulate({
-            age,
-            expenditure
+            age,            // 年齢
+            expenditure,    // 年間の支出
+            value,          // 初期投資額
+            mu,             // リターン
+            sigma,          // リスク
+            money,          // 預金額(万円)
+            salary,         // 年収（手取り)
+            years,          // あと何年働くか
+            invest,         // 年間投資額(万円)
+            emergency,      // 生活防衛費(万円)
         }));
     }
     const result = results[0];
     const winRatio = calcWinRatio(results);
-    const handleChange = (index: number, key:string, value:any) => {
-        let newFunds = funds.slice();
-        if (key === "name") {
-            newFunds[index].name = value;
-        } else if (key === "value") {
-            newFunds[index].value = value;
-        } else if (key === "mu") {
-            newFunds[index].mu = value;
-        } else if (key === "sigma") {
-            newFunds[index].sigma = value;
-        }
-        setFunds(newFunds);
-    }
     return (
         <div>
             
@@ -148,73 +145,93 @@ const App = () => {
                 <span>現在の年齢</span>
                 <span><input type="text"
                     value={age}
-                    onChange={e => setAge(parseInt(e.target.value))}
+                    onChange={e => setAge(parse(e.target.value))}
                 ></input>歳</span>
             </div>
             <div>
                 <span>年間の支出</span>
                 <span><input type="text"
                     value={expenditure}
-                    onChange={e => setExpenditure(parseInt(e.target.value))}
+                    onChange={e => setExpenditure(parse(e.target.value))}
                 ></input>万円</span>
-                <span>(iDeCoや国民年金、健康保険を除く)</span>
             </div>
             <div>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>銘柄</th>
-                            <th>初期投資額(万円)</th>
-                            <th>リターン</th>
-                            <th>リスク</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {funds.map((fund, index) => {
-                        return (
-                            <tr>
-                                <td>
-                                    <input type="text"
-                                        value={fund.name}
-                                        onChange={e => handleChange(index, "name", e.target.value)}
-                                    />
-                                </td>
-                                <td>
-                                    <input type="text"
-                                        value={fund.value}
-                                        onChange={e => handleChange(index, "value", e.target.value)}
-                                    />
-                                </td>
-                                <td>
-                                    <input type="text"
-                                        value={fund.mu}
-                                        onChange={e => handleChange(index, "mu", e.target.value)}
-                                    />
-                                </td>
-                                <td>
-                                    <input type="text"
-                                        value={fund.sigma}
-                                        onChange={e => handleChange(index, "sigma", e.target.value)}
-                                    />
-                                </td>
-                            </tr>
-                        );
-                    })}
-                    </tbody>
-                </table>
+                <span>年収(手取り)</span>
+                <span><input type="text"
+                    value={salary}
+                    onChange={e => setSalary(parse(e.target.value))}
+                ></input>万円</span>
             </div>
+            <div>
+                <span>年間投資額(上限)</span>
+                <span><input type="text"
+                    value={invest}
+                    onChange={e => setInvest(parse(e.target.value))}
+                ></input>万円</span>
+            </div>
+
+            <div>
+                <span>生活防衛資金</span>
+                <span><input type="text"
+                    value={emergency}
+                    onChange={e => setEmergency(parse(e.target.value))}
+                ></input>万円</span>
+            </div>
+            <div>
+                <span>あと何年働くか</span>
+                <span><input type="text"
+                    value={years}
+                    onChange={e => setYears(parse(e.target.value))}
+                ></input>年</span>
+            </div>
+
+
+            <hr/>
+            <div>
+                <span>預金額</span>
+                <span><input type="text"
+                    value={money}
+                    onChange={e => setMoney(parse(e.target.value))}
+                ></input>万円</span>
+            </div>
+            <div>
+                <span>初期投資額</span>
+                <span><input type="text"
+                    value={value}
+                    onChange={e => setValue(parse(e.target.value))}
+                ></input>万円</span>
+            </div>
+            <div>
+                <span>リターン</span>
+                <span><input type="text"
+                    value={mu}
+                    onChange={e => setMu(parseFloat(e.target.value) || 0.0001)}
+                ></input></span>
+            </div>
+            <div>
+                <span>リスク</span>
+                <span><input type="text"
+                    value={sigma}
+                    onChange={e => setSigma(parseFloat(e.target.value) || 0.0001)}
+                ></input></span>
+            </div>
+
+            <hr />
+
             <div>
                 <span>勝率:</span>
                 <span>{Math.floor(100 * winRatio)}%</span>
+                <span>(50年後に資産がマイナスにならない確率)</span>
             </div>
             <div>
                 <table>
                     <thead>
                         <tr>
-                            <th>年数</th>
+                            <th>年目</th>
+                            <th>歳</th>
                             <th>株式</th>
-                            <th>現金</th>
                             <th>リターン(%)</th>
+                            <th>預金額</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -222,9 +239,10 @@ const App = () => {
                         return (
                             <tr>
                                 <td>{record.t}</td>
+                                <td>{record.age}</td>
                                 <td>{Math.floor(record.x)}</td>
-                                <td>{Math.floor(record.y)}</td>
                                 <td>{Math.floor(100 * record.r)}</td>
+                                <td>{Math.floor(record.y)}</td>
                             </tr>
                         );
                     })}
